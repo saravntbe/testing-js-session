13610a4c2999660e36b68a177bc6763fdc289a3e
saravntbe <hi@saravnan.tk>
2022-10-21 10:47:45 +0530

    add slides for basic concepts - testing js session

---
+++ 012-linting-example.js
@@ -0,0 +1,15 @@
+//eslint-disabled
+// any issues with tbe below code ? ?
+
+
+function doSomething(key){return key}
+
+
+if(!'serviceWorker' in navigator) {
+    console.log('Service worker available !')
+}
+
+
+for (key in foo) {
+    doSomething(key);
+}
